44edd0dd05d2d6f877f256c344d18f622626a7cc
bluezebragames <bluezebragames@gmail.com>
2024-03-23 15:25:00 -0700

    add ingredients to stack by drag/drop

--- ts/main.ts
+++ ts/main.ts
@@ -9,12 +9,16 @@ var availableIngredients: string[] = [
 var ingredientCounts: number[] = Array(availableIngredients.length).fill(0);
 const ingredientDiv = document.getElementById("ingredients");
 var ingredientDOMs: HTMLSpanElement[] = [];
+const boardDiv = document.getElementById("myBoard");
+var stackDOMs: HTMLDivElement[] = [];
 
 function constructDOM() {
+    // per ingredient, create a "station" in the left bar from which to drag ingredients
     ingredientDiv.innerHTML = "";
     for (let i in availableIngredients) {
         const div = document.createElement("div");
         div.className = "tooltip";
+        div.draggable = true;
         const ingredientSpan = document.createElement("span");
         div.appendChild(ingredientSpan);
         const tooltipDiv = document.createElement("span");
@@ -27,15 +31,53 @@ function constructDOM() {
         tooltipDiv.innerHTML += `<i>${ing.flavorText}</i>`;
 
         div.onclick = selectCard.bind(null, i);
+        div.ondragstart = dragCard.bind(null, i);
 
         div.appendChild(tooltipDiv);
         ingredientDiv.appendChild(div);
         ingredientDOMs.push(ingredientSpan);
     }
 
+    // add an inital empty stack to add bread, starting a sandwich
+    boardDiv.innerHTML = "";
+    createEmptyStack();
+
     draw();
 
     document.getElementById("drawButton").onclick = drawCards;
+}
+
+function createEmptyStack() {
+    const div = document.createElement("div");
+    
+    div.ondrop = dropHandler.bind(null, 0);
+    div.ondragover = dragOverHandler;
+
+    boardDiv.appendChild(div);
+    stackDOMs.push(div);
+}
+
+// start dragging card to play
+function dragCard(ingId: number, ev: DragEvent) {
+    ev.dataTransfer.setData("ingId", ingId.toString());
+    ev.dataTransfer.dropEffect = "copy";
+}
+
+function dropHandler(stackId: number, event: DragEvent) {
+    event.preventDefault();
+    const ingId = event.dataTransfer.getData("ingId");
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+    const ingName = availableIngredients[ingId];
+    span.innerText = ingredients.get(ingName).name;
+    ingredientCounts[ingId]--;
+    draw();
+    div.appendChild(span);
+    stackDOMs[stackId].insertBefore(div, stackDOMs[stackId].firstChild);
+}
+
+function dragOverHandler(event: DragEvent) {
+    event.preventDefault();
 }
 
 function draw() {
